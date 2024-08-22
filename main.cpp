b6d3dd3079f47bed49e094ffbb10e0b2e3bcca37
kmeeth <matijakup@gmail.com>
2024-08-22 04:51:53 +0200

    Made a functioning addition and subtraction calculator.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include "integer.h"
 #include <iostream>
 #include <unordered_map>
+#include <regex>
 
 using int_titan::integer;
 
@@ -11,13 +12,45 @@ void free_calculator()
         std::cout << "Enter radix:" << std::endl;
         int radix;
         std::cin >> radix;
-        std::cout << "Enter two numbers to add:" << std::endl;
-        std::string x, y;
-        std::cin >> x >> y;
-        integer a = integer::create(x, radix);
-        integer b = integer::create(y, radix);
-        integer r = integer::add(a, b);
-        std::cout << integer::to_string(r, radix, true) << std::endl;
+        std::cout << "Enter the expression (end with '='):" << std::endl;
+        std::string expression;
+        std::cin.ignore();
+        std::getline(std::cin, expression, '=');
+        auto split = [](const std::string& str, const std::string& regex) -> std::vector<std::string>
+        {
+            std::regex r(regex);
+            return {std::sregex_token_iterator(str.begin(), str.end(), r, -1), std::sregex_token_iterator()};
+        };
+        std::vector<std::string> terms = split(expression, R"([+\-\n])");
+        std::vector<char> operations;
+        if(expression[0] != '+' and expression[0] != '-')
+        {
+            operations.emplace_back('+');
+        }
+        for(char c : expression)
+        {
+            if(c == '+' or c == '-')
+            {
+                operations.emplace_back(c);
+            }
+        }
+        auto compute = [](const std::vector<std::string>& terms, const std::vector<char>& operations, const int radix) -> integer
+        {
+            assert(terms.size() == operations.size());
+            integer sum = integer::create("0", radix);
+            for(int i = 0; i < terms.size(); i++)
+            {
+                integer num = integer::create(terms[i], radix);
+                if(operations[i] == '-')
+                {
+                    num = integer::negate(num);
+                }
+                sum = integer::add(sum, num);
+            }
+            return sum;
+        };
+        integer result = compute(terms, operations, radix);
+        std::cout << integer::to_string(result, radix, true) << std::endl;
     }
 }
 
